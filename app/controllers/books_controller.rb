c874b9ddeec330b0a319179946d7a6601652c294
hojokaban <hojokaban@gmail.com>
2020-05-02 17:08:58 +0900

    Fix books create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def show
   	@book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
@@ -11,6 +12,7 @@ class BooksController < ApplicationController
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
   	if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
@@ -43,7 +45,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
